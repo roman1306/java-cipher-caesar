b8679029e2d9ff2cdacc7e19bc96accafd15d243
Roman Petrenko <rpetrenko51@gmail.com>
2020-03-05 20:04:18 +0400

    task сompleted

--- src/CipherCaesar.java
+++ src/CipherCaesar.java
@@ -1,3 +1,19 @@
 public class CipherCaesar {
 
+    public static String decryption(String text, int key) {
+        String alphabet = "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя";
+        StringBuilder textDecryption = new StringBuilder();
+
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isLetter(text.charAt(i))) {
+                int numberSymbol = ((alphabet.indexOf(text.charAt(i)) + 2 * key) % 66);
+                textDecryption.append(alphabet.charAt(numberSymbol));
+            } else {
+                textDecryption.append(text.charAt(i));
+            }
+        }
+
+        System.out.println(textDecryption);
+        return textDecryption.toString();
+    }
 }
